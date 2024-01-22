bcd4455ef4f95e399a2533ad8a323d7cc789c8f6
Alejandro Martínez <alejandromartinezvarel@gmail.com>
2024-01-22 16:14:16 +0100

    feat: pipelines & collisions - fix: background movement

---
+++ assets/js/models/pipeline.js
@@ -0,0 +1,79 @@
+class Pipeline {
+    
+    constructor (ctx, x, y, w, h) {
+        
+        this.ctx = ctx;
+
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        this.vx = BACKGROUND_SPEED;
+        
+        this.sprite = new Image();
+        this.sprite.src = 'assets/img/pipeline.png';
+        
+        this.sprite.onload = () => {
+            this.sprite.isReady = true;
+        }
+    }
+
+    draw() {
+        
+        if (this.sprite.isReady) {
+            this.ctx.drawImage(this.sprite, this.x, this.y, this.w, this.h);
+        }
+    }
+    
+    move(background) {
+        
+        if (background.movements.left) {
+            this.x -= BACKGROUND_SPEED;
+        
+        } else if (background.movements.right) {
+            this.x += BACKGROUND_SPEED;
+        }
+    }
+
+    collidesWithLeft(element) {
+        
+        return (
+            element.x + element.w >= this.x &&
+            element.x + element.w <= this.x + (10 * RF) &&
+            element.y + element.h > this.y + (5 * RF) && 
+            element.y < this.y + this.h - (5 * RF)
+
+        )
+    }
+    
+    collidesWithRight(element) {
+        
+        return (
+            element.x <= this.x + this.w &&
+            element.x  >= this.x + this.w - (10 * RF) &&
+            element.y + element.h > this.y + (5 * RF) &&
+            element.y < this.y + this.h - (5 * RF)
+        )
+    }
+    
+    collidesWithUp(element) {
+
+        return (
+            element.y + element.h > this.y &&
+            element.y + element.h < this.y + (10 * RF)  &&
+            element.x + element.w >= this.x + (5 * RF) &&
+            element.x <= this.x + this.w - (5 * RF)
+        )
+    }
+
+    collidesWithDown(element) {
+
+        return (
+            element.x + element.w >= this.x + (5 * RF) &&
+            element.x <= this.x + this.w - (10 * RF) &&
+            element.y > this.y + this.h - (20 * RF) &&
+            element.y < this.y + this.h 
+        )
+    }
+}
